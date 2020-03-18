4d09ae6f238610c6fc9309e7d21e7177b4fc9a6e
Lnk2past <Lnk2past@gmail.com>
2020-03-18 11:23:31 -0400

    Add function to initialize existing git repo

--- Modules/git/git.psm1
+++ Modules/git/git.psm1
@@ -3,4 +3,10 @@ function Set-Personal-Configuration() {
     invoke-expression -Command "git config user.email Lnk2past@gmail.com"
 }
 
+function Initialize-Git([string]$repository) {
+    invoke-expression -Command "git init ."
+    invoke-expression -Command "git remote add origin $repository"
+    invoke-expression -Command "git pull origin master"
+}
+
 Export-ModuleMember -Function * -Alias *
