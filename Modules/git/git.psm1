f4a7d96251d95275d4253e3a14a0c8d28822fadc
Lnk2past <Lnk2past@gmail.com>
2020-03-20 18:16:00 -0400

    Cleanup

--- Modules/git/git.psm1
+++ Modules/git/git.psm1
@@ -1,9 +1,14 @@
-function Set-Personal-Configuration() {
+function Set-Personal-Configuration {
     invoke-expression -Command "git config user.name Lnk2past"
     invoke-expression -Command "git config user.email Lnk2past@gmail.com"
 }
 
-function Initialize-Git([string]$repository) {
+function Initialize-Git {
+    param(
+        [Parameter(Mandatory=$true,
+        HelpMessage="Enter the URL of the repository you want to initialize here.")]
+        [string]$repository
+    )
     invoke-expression -Command "git init ."
     invoke-expression -Command "git remote add origin $repository"
     invoke-expression -Command "git pull origin master"
--- Modules/python/python.psm1
+++ Modules/python/python.psm1
@@ -1,9 +1,19 @@
-function New-Environment ([string]$env) {
+function New-Environment {
+    param(
+        [Parameter(Mandatory=$true,
+        HelpMessage="Enter the name of an environment to create.")]
+        [string]$env
+    )
     invoke-expression -Command "python -m venv $HOME\Documents\VirtualEnvironments\$env"
-}; Set-Alias activate Enter-Environment
-
-function Enter-Environment ([string]$env) {
-    invoke-expression -Command "$HOME\Documents\VirtualEnvironments\$env\Scripts\Activate.ps1"
 }; Set-Alias mkenv New-Environment
+
+function Enter-Environment {
+    param(
+        [Parameter(Mandatory=$true,
+        HelpMessage="Enter the name of an environment to activate.")]
+        [string]$env
+    )
+    invoke-expression -Command "$HOME\Documents\VirtualEnvironments\$env\Scripts\Activate.ps1"
+}; Set-Alias activate Enter-Environment
 
 Export-ModuleMember -Function * -Alias *
