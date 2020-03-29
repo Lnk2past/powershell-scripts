2ca4d495cf2ec6d1a08385204cf2e107d9c3c3b4
Lnk2past <Lnk2past@gmail.com>
2020-03-29 01:07:13 -0400

    add docker-debug

--- Modules/docker/docker.psm1
+++ Modules/docker/docker.psm1
@@ -10,4 +10,22 @@ function Show-All-Docker {
     docker ps -a
 }
 
+function Debug-Docker {
+    param(
+        [Parameter(Mandatory=$true,
+        HelpMessage="Container to search for")]
+        [String]$containerName,
+
+        [Parameter(Mandatory=$false,
+        HelpMessage="Command to run")]
+        [String]$command = "bash"
+    )
+
+    docker ps -a | select-string -pattern $containerName | ForEach-Object {
+        $_.Line.Split(" ")[0]
+    } | Out-String -OutVariable container | Out-Null
+    $container = $container.Trim()
+    docker exec -it $container $command
+}
+
 Export-ModuleMember -Function * -Alias *
